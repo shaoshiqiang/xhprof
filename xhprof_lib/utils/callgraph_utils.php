0592c6b4bc9bca2ac14f7e52ea5de26798c0caa5
shaoshiqiang <shaoshiqiang@songshuai.com>
2020-03-24 17:56:49 +0800

    fix

--- xhprof_lib/utils/callgraph_utils.php
+++ xhprof_lib/utils/callgraph_utils.php
@@ -107,7 +107,11 @@ function xhprof_generate_image_by_dot($dot_script, $type) {
         2 => array("pipe", "w")
     );
 
-    $cmd = "/home/linuxbrew/.linuxbrew/bin/dot -T".$type;
+    if (PHP_OS == 'Darwin') {
+        $cmd = "/usr/local/bin/dot -T" . $type;
+    } else {
+        $cmd = "/home/linuxbrew/.linuxbrew/bin/dot -T" . $type;
+    }
 
     $process = proc_open( $cmd, $descriptorspec, $pipes, sys_get_temp_dir(), array( 'PATH' => getenv( 'PATH' ) ) );
     if (is_resource($process)) {
