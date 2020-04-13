63df110570a727f580dadacb20270847d4d48165
shaoshiqiang <>
2020-04-13 16:16:55 +0800

    fix

--- xhprof_index/xhprof_index.php
+++ xhprof_index/xhprof_index.php
@@ -1,9 +1,9 @@
 <?php
 $server = $_SERVER;
-
+defined('XHPROF_PATH') || define('XHPROF_PATH', __DIR__ . '/../xhprof_lib/utils');
 if (isset($server['SERVER_NAME']) && strpos($server['SERVER_NAME'], 'xhprof') === false) {
-    include_once "/Users/shaoshiqiang/WWW/xhprof/xhprof_lib/utils/xhprof_lib.php";
-    include_once "/Users/shaoshiqiang/WWW/xhprof/xhprof_lib/utils/xhprof_runs.php";
+    include_once XHPROF_PATH . "/xhprof_lib.php";
+    include_once XHPROF_PATH . "/xhprof_runs.php";
     xhprof_enable();
     //在程序结束后收集数据
     register_shutdown_function(function ($server) {
